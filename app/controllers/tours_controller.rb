7e92b13795ae032bb7804baf4365f4de20006af0
KUMA-06 <hyt_kmkr@icloud.com>
2021-07-28 16:32:02 +0900

    ツアー追加

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,5 +1,24 @@
 class ToursController < ApplicationController
   def index
-    
   end
+
+  def new
+    @tour = Tour.new
+  end
+
+  def create
+    @tour = Tour.create(tour_params)
+    if @tour.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tour_params
+    params.require(:tour).permit(:place)
+  end
+
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,2 +1,5 @@
 class Tour < ApplicationRecord
+  validates :place, presence: true
+
+  # belongs_to :user
 end
